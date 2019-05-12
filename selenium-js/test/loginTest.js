4ca9974ab465535d775e16a0f62a8529edf3f279
Eros Candelaresi <eros@candelaresi.de>
2019-05-12 18:06:13 +0200

    Implement successful login test

--- selenium-js/test/loginTest.js
+++ selenium-js/test/loginTest.js
@@ -2,7 +2,9 @@
 
 var assert = require('assert');
 var webdriver = require('selenium-webdriver'),
-    SeleniumServer = require("selenium-webdriver/remote").SeleniumServer;
+    SeleniumServer = require("selenium-webdriver/remote").SeleniumServer,
+    By = webdriver.By,
+    until = webdriver.until;
 require('chromedriver');
 
 describe('Login', function() {
@@ -28,11 +30,26 @@ describe('Login', function() {
         // without higher timeout Mocha will abort tests that run longer than 2s
         this.timeout(20000);
 
-        await driver.manage().setTimeouts({ implicit: 10000 });
-        driver.get(loginPage)
+        driver.manage().setTimeouts({ implicit: 10000 });
+        await driver.get(loginPage)
+
+        let userField = await driver.findElement(By.id("username"));
+        let passwordField = await driver.findElement(By.id("password"));
+        let submitButton = await driver.findElement(By.id("loginbtn"));
+
+        await userField.sendKeys("admin");
+        await passwordField.sendKeys("sandbox");
+        await submitButton.click();
+
+
+        await driver.wait(until.elementLocated(By.className("usertext")));
+        let username = await driver.findElement(By.className("usertext")).getText();
+
+        assert.equal("Admin Userr", username);
     });
 
     it('should fail with incorrect credentials', function() {
+        this.timeout(20000);
         //assert.equal(false, true);
     });
 });
