2e78507c107b45eeac766ef94a94421250ea59cb
Eros Candelaresi <eros@candelaresi.de>
2019-05-12 16:42:56 +0200

    Perform login on a public demo website

--- selenium-csharp/selenium-csharp/Program.cs
+++ selenium-csharp/selenium-csharp/Program.cs
@@ -10,7 +10,27 @@ namespace selenium_csharp
         {
             ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"lib", "chromedriver");
             IWebDriver driver = new ChromeDriver(service);
-            driver.Navigate().GoToUrl("http://google.com");
+
+            // set driver to wait for every page to load, before continuing
+	    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl("https://demo.moodle.net/login/index.php");
+	    IWebElement loginBox = driver.FindElement(By.Id("username"));
+	    IWebElement passwordBox = driver.FindElement(By.Id("password"));
+	    IWebElement submitButton = driver.FindElement(By.Id("loginbtn"));
+
+	    loginBox.SendKeys("admin");
+	    passwordBox.SendKeys("sandbox");
+	    submitButton.Click();
+
+	    IWebElement userText = driver.FindElement(By.ClassName("usertext"));
+	    String username = userText.Text;
+
+	    if (username.Equals("Admin User")) {
+		    System.Console.WriteLine("Test passed!");
+	    } else {
+		    System.Console.WriteLine("Test failed!");
+	    }
         }
     }
 }
