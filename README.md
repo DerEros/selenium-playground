f39782f380fa7b632c173498b45ff8ba9faf982d
Eros Candelaresi <eros@candelaresi.de>
2019-05-12 15:32:05 +0200

    Start Chrome and navigate to a page

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# selenium-playground
+# Selenium Playground
 Experimenting with Selenium UI testing on different programming languages.
+
+# Todos
+
+- [ ] Selenium C#
+  - [x] Set up build infrastructure
+  - [x] Fire up browser
+  - [ ] Log into some website
+- [ ] Selenium Javascript
+  - [ ] Set up build infrastructure
+  - [ ] Fire up browser
+  - [ ] Log into some website
---
+++ selenium-csharp/selenium-csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"lib", "chromedriver");
+	    IWebDriver driver = new ChromeDriver(service);
+	    driver.Navigate().GoToUrl("http://google.com");
+        }
+    }
+}
